b65ee66278e4b5510b5e85f1b099f90fe9deb9b4
gorasomasvet-spec <gorasomasvet@gmail.com>
2026-06-08 13:21:10 +0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,31 +1,83 @@
 import asyncio
 import os
 
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import (
+    Message,
+    ReplyKeyboardMarkup,
+    KeyboardButton
+)
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
-
-if not BOT_TOKEN:
-    raise ValueError("BOT_TOKEN не найден в Variables Railway")
 
 bot = Bot(token=BOT_TOKEN)
 dp = Dispatcher()
 
+menu = ReplyKeyboardMarkup(
+    keyboard=[
+        [KeyboardButton(text="🔍 Найти песню")],
+        [KeyboardButton(text="📜 История")],
+        [KeyboardButton(text="🎵 Мой плейлист")]
+    ],
+    resize_keyboard=True
+)
+
+history = {}
 
 @dp.message(Command("start"))
 async def start(message: Message):
     await message.answer(
-        "🎵 Бот работает!\n\n"
-        "Кнопки будут добавлены позже."
+        "🎵 Music Bot\n\nВыберите действие:",
+        reply_markup=menu
     )
 
+@dp.message(F.text == "🔍 Найти песню")
+async def search_song(message: Message):
+    await message.answer(
+        "Напиши название песни:"
+    )
+
+@dp.message(F.text == "📜 История")
+async def show_history(message: Message):
+    user_id = message.from_user.id
+
+    songs = history.get(user_id, [])
+
+    if not songs:
+        await message.answer("История пуста")
+        return
+
+    text = "\n".join(songs)
+
+    await message.answer(
+        f"📜 История:\n\n{text}"
+    )
+
+@dp.message(F.text == "🎵 Мой плейлист")
+async def playlist(message: Message):
+    await message.answer(
+        "🎵 Пока плейлист пуст"
+    )
+
+@dp.message()
+async def handle_text(message: Message):
+    user_id = message.from_user.id
+
+    if user_id not in history:
+        history[user_id] = []
+
+    history[user_id].append(message.text)
+
+    await message.answer(
+        f"🔎 Ищу песню: {message.text}"
+    )
 
 async def main():
-    print("BOT STARTED")
-    await dp.start_polling(bot)
+    me = await bot.get_me()
+    print(f"Авторизован как @{me.username}")
 
+    await dp.start_polling(bot)
 
 if __name__ == "__main__":
     asyncio.run(main())
